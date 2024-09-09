e7d091b219e3bc97aa84dc7bc4227ae67706361d
latif <latifyousuf513@gmail.com>
2024-09-09 21:59:04 +0500

    first commit

--- app.js
+++ app.js
@@ -1,27 +1,109 @@
-var _a;
-(_a = document.getElementById("generateResume")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
-    var mainDiv = document.getElementById("mainDiv");
-    var generateResume = document.getElementById("generateResume");
-    var form = document.getElementById("form");
-    var subBtn = document.getElementById("subBtn");
-    mainDiv === null || mainDiv === void 0 ? void 0 : mainDiv.style.display = "none";
-    generateResume === null || generateResume === void 0 ? void 0 : generateResume.style.display = "none";
+var mainDiv = document.getElementById("mainDiv");
+var generateResume = document.getElementById("generateResume");
+var form = document.getElementById("form");
+var subBtn = document.getElementById("subBtn");
+var generatePDF = document.getElementById("generatePDF");
+generateResume === null || generateResume === void 0 ? void 0 : generateResume.addEventListener("click", function () {
+    mainDiv.style.display = "none";
+    generateResume.style.display = "none";
     form.style.display = "flex";
-    subBtn === null || subBtn === void 0 ? void 0 : subBtn.style.display = "flex";
+    subBtn.style.display = "flex";
 });
-// let file = document.getElementById("propic")
-// file?.onchange = () =>{
-//     console.log(file.files[0])
-// }
-// let reader = new FileReader()
-// reader.readAsDataURL(file)
-// console.log(reader.result)
-// window.onload = function () {
-//     document.getElementById("generatePDF")
-//     ?.addEventListener("click", ()=>{
-//         const mainDiv = document.getElementById("mainDiv");
-//         console.log(mainDiv?.innerHTML)
-//         console.log(window)
-//         html2pdf().from(mainDiv).save()
-//     })
-// }
+var formSkill = document.getElementById("formSkill");
+var paraSkills = document.getElementById("paraSkills");
+var formEdu = document.getElementById("formEdu");
+var eduPara = document.getElementById("eduPara");
+var lanInput = document.getElementById("lanInput");
+var lanPara = document.getElementById("lanPara");
+var nameHeading = document.getElementById("nameHeading");
+var nameInput = document.getElementById("nameInput");
+var desHeading = document.getElementById("desHeading");
+var ndoDesInput = document.getElementById("ndoDesInput");
+var formObj = document.getElementById("formObj");
+var objPara = document.getElementById("objPara");
+var email = document.getElementById("email");
+var formEmail = document.getElementById("formEmail");
+var cNumber = document.getElementById("cNumber");
+var formNumber = document.getElementById("formNumber");
+var address = document.getElementById("address");
+var addInput = document.getElementById("addInput");
+var facebook = document.getElementById("facebook");
+var fbInput = document.getElementById("fbInput");
+var linkedin = document.getElementById("linkedin");
+var ldInput = document.getElementById("ldInput");
+var gHub = document.getElementById("gHub");
+var ghInput = document.getElementById("ghInput");
+var iGram = document.getElementById("iGram");
+var instaInput = document.getElementById("instaInput");
+var desWe = document.getElementById("des-we");
+var desInput = document.getElementById("desInput");
+var desOrg = document.getElementById("des-org");
+var orgInput = document.getElementById("orgInput");
+var durHead = document.getElementById("durHead");
+var durInput = document.getElementById("durInput");
+var jobAdd = document.getElementById("jobAdd");
+var orgAddInput = document.getElementById("orgAddInput");
+var weJdp = document.getElementById("we-jdp");
+var jdInput = document.getElementById("jdInput");
+var coursePara = document.getElementById("coursePara");
+var courseInput = document.getElementById("courseInput");
+var submitBtn = document.getElementById("submitBtn");
+var img = document.getElementById("img");
+// Get the file input element and cast it as an HTMLInputElement
+var fileInput = document.getElementById("propic");
+fileInput.onchange = function () {
+    if (fileInput.files && fileInput.files[0]) {
+        var file = fileInput.files[0];
+        console.log(file);
+        var reader_1 = new FileReader();
+        reader_1.onload = function () {
+            // The result is a data URL that can be assigned to img.src
+            img.src = reader_1.result;
+        };
+        // Read the file as a Data URL (base64 string)
+        reader_1.readAsDataURL(file);
+    }
+};
+submitBtn.addEventListener("click", function () {
+    paraSkills.innerHTML = formSkill.value;
+    eduPara.innerHTML = formEdu.value;
+    lanPara.innerHTML = lanInput.value;
+    nameHeading.innerHTML = nameInput.value;
+    desHeading.innerHTML = ndoDesInput.value;
+    objPara.innerHTML = formObj.value;
+    email.innerHTML = formEmail.value;
+    cNumber.innerHTML = formNumber.value;
+    address.innerHTML = addInput.value;
+    facebook.href = fbInput.value;
+    linkedin.href = ldInput.value;
+    gHub.href = ghInput.value;
+    iGram.href = instaInput.value;
+    desWe.innerHTML = desInput.value;
+    desOrg.innerHTML = orgInput.value;
+    durHead.innerHTML = durInput.value;
+    jobAdd.innerHTML = orgAddInput.value;
+    weJdp.innerHTML = jdInput.value;
+    coursePara.innerHTML = courseInput.value;
+    mainDiv.style.display = "flex";
+    generateResume.style.display = "none";
+    generatePDF.style.display = "flex";
+    form.style.display = "none";
+    subBtn.style.display = "none";
+});
+//  PDF FUNCTION
+window.onload = function () {
+    if (generatePDF) {
+        generatePDF.addEventListener("click", function () {
+            if (mainDiv) {
+                console.log(mainDiv.innerHTML);
+                html2pdf().from(mainDiv).save();
+            }
+            else {
+                console.log("mainDiv element not found.");
+            }
+        });
+    }
+    else {
+        console.log("generatePDF element not found.");
+    }
+};
--- app.ts
+++ app.ts
@@ -1,31 +1,120 @@
 
-    document.getElementById("generateResume")
-    ?.addEventListener("click", () => {
-        const mainDiv = document.getElementById("mainDiv") as HTMLElement
-        const generateResume = document.getElementById("generateResume")
-        const form = document.getElementById("form") as HTMLElement
-        const subBtn = document.getElementById("subBtn")
-        mainDiv?.style.display = "none";
-        generateResume?.style.display = "none"
+
+const mainDiv = document.getElementById("mainDiv") as HTMLElement
+const generateResume = document.getElementById("generateResume") as HTMLElement
+const form = document.getElementById("form") as HTMLElement
+const subBtn = document.getElementById("subBtn") as HTMLElement
+const generatePDF = document.getElementById("generatePDF") as HTMLElement
+
+
+    generateResume?.addEventListener("click", () => {
+        mainDiv.style.display = "none";
+        generateResume.style.display = "none"
         form.style.display = "flex"
-        subBtn?.style.display = "flex"
+        subBtn.style.display = "flex"
     })
+    let formSkill = document.getElementById("formSkill") as HTMLInputElement
+    let paraSkills = document.getElementById("paraSkills") as HTMLElement
+    let formEdu = document.getElementById("formEdu") as HTMLInputElement
+    let eduPara = document.getElementById("eduPara") as HTMLElement
+    let lanInput = document.getElementById("lanInput") as HTMLInputElement
+    let lanPara = document.getElementById("lanPara") as HTMLElement
+    let nameHeading = document.getElementById("nameHeading") as HTMLElement
+    let nameInput = document.getElementById("nameInput") as HTMLInputElement
+    let desHeading = document.getElementById("desHeading") as HTMLElement
+    let ndoDesInput = document.getElementById("ndoDesInput") as HTMLInputElement
+    let formObj = document.getElementById("formObj") as HTMLInputElement
+    let objPara = document.getElementById("objPara") as HTMLElement
+    let email = document.getElementById("email") as HTMLElement
+    let formEmail = document.getElementById("formEmail") as HTMLInputElement
+    let cNumber = document.getElementById("cNumber") as HTMLElement
+    let formNumber = document.getElementById("formNumber") as HTMLInputElement
+    let address = document.getElementById("address") as HTMLElement
+    let addInput = document.getElementById("addInput") as HTMLInputElement
+    let facebook = document.getElementById("facebook") as HTMLAnchorElement
+    let fbInput = document.getElementById("fbInput") as HTMLInputElement
+    let linkedin = document.getElementById("linkedin") as HTMLAnchorElement
+    let ldInput = document.getElementById("ldInput") as HTMLInputElement
+    let gHub = document.getElementById("gHub") as HTMLAnchorElement
+    let ghInput = document.getElementById("ghInput") as HTMLInputElement
+    let iGram = document.getElementById("iGram") as HTMLAnchorElement
+    let instaInput = document.getElementById("instaInput") as HTMLInputElement
+    let desWe = document.getElementById("des-we") as HTMLElement
+    let desInput = document.getElementById("desInput") as HTMLInputElement
+    let desOrg = document.getElementById("des-org") as HTMLElement
+    let orgInput = document.getElementById("orgInput") as HTMLInputElement
+    let durHead = document.getElementById("durHead") as HTMLElement
+    let durInput = document.getElementById("durInput") as HTMLInputElement
+    let jobAdd = document.getElementById("jobAdd") as HTMLElement
+    let orgAddInput = document.getElementById("orgAddInput") as HTMLInputElement
+    let weJdp = document.getElementById("we-jdp") as HTMLElement
+    let jdInput = document.getElementById("jdInput") as HTMLInputElement
+    let coursePara = document.getElementById("coursePara") as HTMLElement
+    let courseInput = document.getElementById("courseInput") as HTMLInputElement
+    let submitBtn = document.getElementById("submitBtn") as HTMLElement
+    let img = document.getElementById("img") as HTMLImageElement
 
-    // let file = document.getElementById("propic")
-    // file?.onchange = () =>{
-    //     console.log(file.files[0])
-    // }
-    // let reader = new FileReader()
-    // reader.readAsDataURL(file)
-    // console.log(reader.result)
+// Get the file input element and cast it as an HTMLInputElement
+let fileInput = document.getElementById("propic") as HTMLInputElement;
+fileInput.onchange = () => {
+    if (fileInput.files && fileInput.files[0]) {
+        let file = fileInput.files[0];
+        console.log(file)
+        let reader = new FileReader();
+
+        reader.onload = () => {
+            // The result is a data URL that can be assigned to img.src
+            img.src = reader.result as string;
+        };
+
+        // Read the file as a Data URL (base64 string)
+        reader.readAsDataURL(file);
+    }
+};
+
+
+    submitBtn.addEventListener("click", () => {
+        paraSkills.innerHTML = formSkill.value;
+        eduPara.innerHTML = formEdu.value;
+        lanPara.innerHTML = lanInput.value;
+        nameHeading.innerHTML = nameInput.value;
+        desHeading.innerHTML = ndoDesInput.value;
+        objPara.innerHTML = formObj.value;
+        email.innerHTML = formEmail.value;
+        cNumber.innerHTML = formNumber.value;
+        address.innerHTML = addInput.value;
+        facebook.href = fbInput.value;
+        linkedin.href = ldInput.value;
+        gHub.href = ghInput.value;
+        iGram.href = instaInput.value;
+        desWe.innerHTML = desInput.value;
+        desOrg.innerHTML = orgInput.value;
+        durHead.innerHTML = durInput.value;
+        jobAdd.innerHTML = orgAddInput.value;
+        weJdp.innerHTML = jdInput.value;
+        coursePara.innerHTML = courseInput.value;
+
+        mainDiv.style.display = "flex";
+        generateResume.style.display = "none";
+        generatePDF.style.display = "flex";
+        form.style.display = "none";
+        subBtn.style.display = "none";
+})
     
+    //  PDF FUNCTION
 
-// window.onload = function () {
-//     document.getElementById("generatePDF")
-//     ?.addEventListener("click", ()=>{
-//         const mainDiv = document.getElementById("mainDiv");
-//         console.log(mainDiv?.innerHTML)
-//         console.log(window)
-//         html2pdf().from(mainDiv).save()
-//     })
-// }
+    window.onload = function () {
+        if (generatePDF) {
+            generatePDF.addEventListener("click", () => {
+                if (mainDiv) {
+                    console.log(mainDiv.innerHTML);
+                    html2pdf().from(mainDiv).save();
+                } else {
+                    console.log("mainDiv element not found.");
+                }
+            });
+        } else {
+            console.log("generatePDF element not found.");
+        }
+    };
+    
